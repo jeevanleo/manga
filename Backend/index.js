270e85ee0fb26779235d7d33077da6b0bd0e8de9
Jeevan Babu Satyam <116184803+jeevanleo@users.noreply.github.com>
2024-05-07 21:08:53 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(cors(
   {
-    origin: ["https://manga-frontend-chi.vercel.app/"],
+    origin: ["https://manga-frontend-chi.vercel.app"],
     methods: ["POST", "GET"],
     credentials: true
   }
